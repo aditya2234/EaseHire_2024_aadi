d78aedf2f225f650d13e95e2da21ef57108fd79f
Gourab Chatterjee <114818987+Gourab-Kr-Chatterjee@users.noreply.github.com>
2024-11-06 18:38:40 +0530

    final changes 11-06 6:38pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
+
 import { SkillManagementComponent } from './components/skill-management/skill-management.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { CandidateHomeComponent } from './components/candidate-home/candidate-home.component';
 import { InterviewerHomeComponent } from './components/interviewer-home/interviewer-home.component';
 import { CandidateService } from './services/candidate.service';
-import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent,
