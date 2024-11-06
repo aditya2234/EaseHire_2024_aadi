917f45d8e05c017991ba9929ca1761ea5b87da85
Avishek Kundu <avishek.10744263@ltimindtree.com>
2024-11-06 17:44:45 +0530

    Navbar Styling AK 17-44

---
+++ src/app/services/navbar.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarService {
+
+
+  constructor() { }
+  navbar_options = {
+    option1: {
+      home: "Home",
+      assessment: "Assessment",
+      interview: "Interview",
+      schedule: "Schedule"
+    },
+    option2: {
+      home: "Home",
+      marks: "Marks",
+      assessment: "Assessment",
+      schedule: "Schedule"
+    }
+  };
+}
+
+
